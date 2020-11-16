82a115864dd88333d7e99b9fe0a80ead61d70244
Sanders <patternsanders@gmail.com>
2020-11-16 14:29:50 -0600

    install react

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product_list = [
+  ["0000001", "Tylenol", "4.99", "1", "Tylenol is a pain reliever and a fever reducer. Tylenol is used to treat many conditions such as headache, muscle aches, arthritis, backache, toothaches, sore throats, colds, flu, and fevers.", "", "", "Pain Relief", "5000"],
+  ["0000002", "Advil", "4.99", "1", "Advil is used to relieve pain from various conditions such as headache, dental pain, menstrual cramps, muscle aches, or arthritis.", "", "", "Pain Relief", "5000"],
+  ["0000003", "Pepto Bismol", "11.99", "12", "Nausea, heartburn, indigestion, upset stomach, and diarrhea.", "", "", "Indegestion", "179"],
+  ["0000004", "Reactine", "18.99", "1", "Say NO to allergies", "", "", "Allergies", "5652"],
+  ["0000005", "Claritin", "17.99", "2", "Claritin Clear BABY", "", "", "Allergies", "7"],
+  ["0000006", "Ceterizine", "9.99", "1", "It's generic, it does the exact same thing but it's cheaper", "", "", "Allergies", "99"],
+  ["0000007", "Gravol", "9.99", "5", "Please don't abuse this", "", "", "Sleep Aid", "51"],
+  ["0000008", "Melatonin", "8.99", "3", "Sleep like a baby", "", "", "Sleep Aid", "999"],
+  ["0000009", "Aleve", "9.99", "1", "It helps with your allergies", "", "", "Allergies", "789"],
+  ["00000010", "Midol", "5.99", "5", "Live Life M-Powered", "", "", "Pain Relief", "785"]
+]
+
+product_list.each do |sku, name, price, weight, description, thumbnail, image, category, stock|
+  Product.create!(
+    sku: sku,
+    name: name,
+    price: price,
+    weight: weight,
+    description: description,
+    thumbnail: thumbnail,
+    image: image,
+    category: category,
+    stock: stock
+  )
+end
